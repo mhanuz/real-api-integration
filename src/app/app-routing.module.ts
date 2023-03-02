990971a5eb2fe3cf4dc3abfc06c08cccfcb4bde5
Anuz <ceanuz66@gmail.com>
2023-03-02 08:31:52 +0600

    add home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path:'pages', loadChildren:()=>import('./pages/pages.module').then(m=>m.PagesModule)},
+  {path:'', loadChildren:()=>import('./pages/pages.module').then(m=>m.PagesModule)},
   {path:'login',component:LoginComponent}
 ];
 
